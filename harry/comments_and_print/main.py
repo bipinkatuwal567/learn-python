9aa35008d044d6e4a64963fbf2b2bcf81d09cdbb
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-30 17:35:54 +0545

    comments and print

---
+++ harry/comments_and_print/main.py
@@ -0,0 +1,14 @@
+# This is single a line of comment
+# Part of coding life, programmer doesn't want to execute
+
+"""
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+"""
+
+print("Hey it's me Bipin katuwal.\nI'm from \"Nepalgunj\", Nepal.")
+
+
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+print("Hey i am python", 6, "and python", sep="~", end="0007")
