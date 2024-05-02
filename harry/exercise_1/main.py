5ceac609a59eeb74767206161e0242dc9035cf82
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-02 20:37:18 +0545

    done exercise 1

---
+++ harry/exercise_1/main.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+add = a + b
+print("Addition of ", a , " and ", b , " is ", add)
+
+sub = a - b
+print("Subtraction of ", a , " and ", b , " is ", sub)
+
+multiply = a * b
+print("Multiplication of ", a , " and ", b , " is ", multiply)
+
+division = a / b
+print("Division of ", a , " and ", b , " is ", division)
