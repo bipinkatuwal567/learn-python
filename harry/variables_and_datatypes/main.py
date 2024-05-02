fc173b1832472ebeaad0337e8faefff4d78c7e0f
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-02 20:07:57 +0545

    learnt about variable and data ytpes

---
+++ harry/variables_and_datatypes/main.py
@@ -0,0 +1,16 @@
+a = 123 # int type
+b = True # boolen type
+c = "string" # string type
+d = None # none type
+
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+
+# Data types
+# Numeric data : (int, float, complex)
+# Text Data : str
+# Boolean Data : true or false
+# Sequenced data: list, tuple
+# Mapped data: dict
