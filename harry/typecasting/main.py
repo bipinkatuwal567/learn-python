2b78b9212fea17c3b5a9bddfc02b39e0cfc324d6
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-11 22:05:43 +0545

    learn typecating

---
+++ harry/typecasting/main.py
@@ -0,0 +1,22 @@
+# The conversion of one data type into the other data type is known as type casting in python or type conversion in python
+
+# Two Types of Typecasting in Python:
+
+# 1. Explicit Conversion
+# Explicit conversion means conversion of one data type into another data type done via developer or programmer's intervention or manually as per the requirement.
+
+# 2. Implicit Conversion
+# Data types in Python do not have the same leve i.e. ordering of data types is not the same in Python. Some of the data types have higher-order, and some have lower order. 
+
+x = int(1); # x will be 1
+y = int(2.8); # y will be 2
+z = int("3"); # z will be 3
+
+print(x, y, z);
+
+
+# Implicit TypeCasting
+c = 1.9;
+d = 8;
+
+print(c + d);
