a9567c059fc865ff6108794b31e6ee30aef3d753
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-11 22:16:55 +0545

    learn how to take input from user

---
+++ harry/user_input/main.py
@@ -0,0 +1,10 @@
+#In Python, we can take user input using input() function. This input function gives a return value as string/character hence we have to pass that into a variable
+
+#Python stops executing when it comes to the input() function, and continues when the user has given some input.
+
+username = input("Enter your username: ")
+print("Your username is ",username)
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+print(x + y)
