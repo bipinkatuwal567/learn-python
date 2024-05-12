fc74b49d55b14a3f2874485b66905a12d06b42ce
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-12 21:24:12 +0545

    learn strings in python

---
+++ harry/strings/main.py
@@ -0,0 +1,30 @@
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+username = "forest"
+print(username)
+
+# We can use quotes inside a string, as long as they don't match the quotes surrounding the string
+print("Hello, I'm Bipin Katuwal!")
+
+# Multiline Strings
+# We can assign a multiline string to a variable by using three quotes:
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+"""
+print(a);
+
+# Accessing Characters in a String
+print(username[0]) # It will print f
+
+# Looping through the string
+for charater in username:
+    print(charater);
+
+# Check String
+# To check if a certain phrase or character is present in a string, we can use the keyword in.
+
+txt = "The best things in life are free!"
+print("free" in txt) # It will print true
