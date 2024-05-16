f4f91547b5e1039514cbb17faea94ef191f45f93
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-16 22:24:26 +0545

    learn while loop

---
+++ harry/while_loop/main.py
@@ -0,0 +1,8 @@
+i = 0
+while i < 3:
+    print(i)
+    i = i + 1
+
+
+# Else with While loop
+# We can use the the else statement with the while loop. Essentially what the else statement does is that as soon as the while loop condition become false, the interpreter comes out of the while loop and the else statement is executed
