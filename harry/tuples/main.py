b7566837d92d8eba81185b1ad38b83a1a3e8b538
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-21 20:27:46 +0545

    learn tuples

---
+++ harry/tuples/main.py
@@ -0,0 +1,8 @@
+# Tuples
+# Tuples are ordered collection of data items. They store multiple items in a single variable. Tuple items are separated by commas and enclosed within round brackets (). Tuples are unchangable meaning we can not alter them after creation. 
+
+tuple1 = (1,)
+print(type(tuple1))
+
+details = ("Forest", 21, "ASMT")
+print(details)
