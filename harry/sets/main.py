e1a97b108876367d9dfc2b83cb2855e8095d3328
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-05-26 22:33:03 +0545

    learn set

---
+++ harry/sets/main.py
@@ -0,0 +1,11 @@
+# Sets are unordered collection of data items. They store items in a single variable. Set items are separated by commas and enclosed within curly brackets{}. Sets are unchangable, meaning you cannot change items of the set once created. Sets do not contain duplicate items.
+
+thisSets = {2,3,5,6,2,3}
+print(thisSets)
+
+info = {"Roberto", 56, False, 5.9, 100}
+print(info)
+
+# varSet = {} This will output dict
+varSet = set()
+print(type(varSet))
