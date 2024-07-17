a5033a4dfc1c8f3f3fce77bcbd11ba210a56f3d3
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-07-17 22:16:40 +0545

    learn read and realines in file IO

---
+++ harry/file_IO/readLines.py
@@ -0,0 +1,18 @@
+f = open("test.txt")
+i = 0
+while True: 
+    i = i + 1
+    line = f.readline()    
+    if not line: 
+        break
+    
+    m1 = line.split(",")[0]
+    m2 = line.split(",")[1]
+    m3 = line.split(",")[2]
+
+    print(f"Marks of student {i} in Maths is: {m1}")
+    print(f"Marks of student {i} in Science is: {m2}")
+    print(f"Marks of student {i} in English is: {m3}")
+    print(line)
+
+# print("Hello")
---
+++ harry/file_IO/test.py
@@ -0,0 +1,7 @@
+thisList = [1,2,3,4,9,18,19]
+
+count = 0
+for num in thisList:
+    if num % 2 == 0: 
+        count += 1
+print(count)
